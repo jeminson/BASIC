c431a4485d7ff00ef699234680774faa5e8f0f50
Je Min Son <jeminson0802@gamil.com>
2018-10-31 21:46:59 -0500

    Main storyboard

---
+++ BASIC/BASIC/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BASIC
+//
+//  Created by Je Min Son on 2018. 10. 31..
+//  Copyright © 2018년 Je Min Son. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
