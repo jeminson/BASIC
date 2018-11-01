6b20bc4363ed7c38d0544b2b925738c6602cac55
Je Min Son <jeminson0802@gamil.com>
2018-10-31 21:54:51 -0500

    added RoundButton

---
+++ BASIC/BASIC/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  BASIC
+//
+//  Created by Je Min Son on 2018. 10. 31..
+//  Copyright © 2018년 Je Min Son. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+}
