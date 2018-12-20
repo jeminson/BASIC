c3010756cd382dc9d9e9bda8671237ef58e72935
Je Min Son <jeminson@Jeui-MacBook-Pro-3.local>
2018-12-20 15:28:22 -0600

    init

---
+++ QuickAndNimbleUnitTestingDemo/QuickAndNimbleUnitTestingDemo/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickAndNimbleUnitTestingDemo
+//
+//  Created by Je Min Son on 12/20/18.
+//  Copyright © 2018 Jason Son. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ QuickAndNimbleUnitTestingDemo/QuickAndNimbleUnitTestingDemoTests/ModelTest/CurrentWeatherSpecs.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeatherSpecs.swift
+//  QuickAndNimbleUnitTestingDemoTests
+//
+//  Created by Je Min Son on 12/20/18.
+//  Copyright © 2018 Jason Son. All rights reserved.
+//
+
+import Foundation
